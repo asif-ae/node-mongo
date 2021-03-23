0f1869573dcfb4263e8d2d1254aed0b3190d662e
Asif Estiak <me.asif.ae@gmail.com>
2021-03-23 16:27:46 +0600

    finished the module

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
+// parse application/json
+app.use(bodyParser.json()); 
 
 app.get('/', (req, res) => {
   const fruits = {
@@ -20,13 +23,18 @@ const users = ["Asif", "Es", "Linn", "Al", "Ai", "It"];
 
 app.get('/users/:id', (req, res) => {
   const id = req.params.id;
-  console.log(req.query.sort);
   const name = users[id];
   res.send({id, name});
 });
 
+
+// Post
 app.post('/addUser', (req, res) => {
-  console.log('Post request recived')
+  console.log('Data recived', req.body);
+  // Send to database
+  const user = req.body;
+  user.id = 55;
+  res.send(user);
 });
 
 app.listen(3000, () => console.log('Litsening to port 3000'))

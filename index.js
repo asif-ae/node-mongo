42521f51b6eb4bd703b15063d004daaff91fa7c2
Asif Estiak <me.asif.ae@gmail.com>
2021-03-23 14:00:11 +0600

    added some code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
-
-const app = express()
+const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
   const fruits = {
@@ -15,13 +16,17 @@ app.get('/fruits/', (req, res) => {
   res.send({fruits: 'banana', quantity: 1000, price: 10000})
 });
 
+const users = ["Asif", "Es", "Linn", "Al", "Ai", "It"];
 
 app.get('/users/:id', (req, res) => {
-  const users = ["Asif", "Es", "Linn", "Al", "Ai", "It"];
   const id = req.params.id;
   console.log(req.query.sort);
   const name = users[id];
   res.send({id, name});
+});
+
+app.post('/addUser', (req, res) => {
+  console.log('Post request recived')
 });
 
 app.listen(3000, () => console.log('Litsening to port 3000'))
